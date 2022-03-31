7bb3ef623e346d7e193b7d818c1698e4bbdd88fa
omarr12345 <omarr12345omar15@gmail.com>
2022-03-31 16:23:14 +0200

    update

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ var adminAddProductsApi = "/api/admin/addproduct";
 
 //Database Connection
 var connection = mysql.createConnection({
-  host: "45.93.137.66",
+  host: "localhost",
   user: "username",
   password: "password",
   database: "Product",
