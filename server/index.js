36cc783bef4b968a9b3102f5cbcd5ea5f7061dd3
omarr12345 <omar12345omar15@gmail.com>
2022-08-06 18:18:47 +0200

    changing profit withdrwal lik

--- server/index.js
+++ server/index.js
@@ -622,7 +622,10 @@ app.post("/api/profitwithdrawalemail", authenticate, (req, res) => {
 
       const token = createToken(result[0].Id);
       var href =
-        process.env.REACT_APP_URLL + "/withdrawalconfirmation/" + token + "";
+        process.env.REACT_APP_API_URLL +
+        "/withdrawalconfirmation/" +
+        token +
+        "";
       res.json(result[0]);
       var mailOptions = {
         from: "noreply@bee3now.com",
